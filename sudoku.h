32fafea46a82ed93fbce492656cf8d8b26dac268
varungirish97 <varun97girish@gmail.com>
2025-05-30 16:44:12 -0700

    Added a puzzle header file with the puzzle class

--- sudoku.h
+++ sudoku.h
@@ -4,3 +4,12 @@
 #include <vector>
 
 void printPuzzle(std::vector<std::vector<int>> puzzle);
+
+
+/*
+
+Define two classes, one for each grid and one for the entire sudoku.
+Add elements like row, column, number, code etc to the grid.
+
+
+*/
